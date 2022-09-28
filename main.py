eba536bfe894243d84f4f4ef5150dd2591982cf2
Isaac Rodriguez <isaac.bh.rod@gmail.com>
2022-09-28 17:55:52 -0400

    added reverser text,mult,and range within function

--- main.py
+++ main.py
@@ -1,8 +1,40 @@
-# max_num()t
+# max_num()
 
 def max_num(a,b,c):
   return max([a,b,c])
 
-print(max_num(1,2,3))
-print(max_num(100,50,1))
-print(max_num(15,30,2))
+print(max_num(5,10,15))
+print(max_num(100,69,3))
+print(max_num(71,30,8))
+
+# mult_list()
+def mult_list(lst):
+  if len(lst) == 0:
+    return 0
+  prod = lst[0]
+
+  if len(lst) > 1:
+    for i in lst[1:]:
+      prod = prod * i
+
+  return prod
+
+print(mult_list([2,4,6]))
+print(mult_list([]))
+print(mult_list([11]))
+
+#rev_string()
+def rev_string(my_str):
+  return my_str[::-1]
+
+print(rev_string("Racecar"))
+print(rev_string("Hannah"))
+print(rev_string("Bob"))
+
+#num_within()
+def num_within(x,a,b):
+  return x in range(a,b+1)
+     
+print(num_within(1,2,3))     
+print(num_within(5,10,15))     
+print(num_within(69,13,69))
