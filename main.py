df2c5e4d72630ee970f648d4983e6b6be88416ef
Isaac Rodriguez <isaac.bh.rod@gmail.com>
2022-09-28 18:02:49 -0400

    pascal function

--- main.py
+++ main.py
@@ -38,3 +38,35 @@ def num_within(x,a,b):
 print(num_within(1,2,3))     
 print(num_within(5,10,15))     
 print(num_within(69,13,69))
+
+#pascal()
+triangle = [[1],[1,1]]
+def pascal(n):
+  if n < 1:
+    print("invalid number of rows")
+  elif n == 1:
+    print(triangle[0])
+  else:
+    row_number = 2
+    while len(triangle) < n:
+      row = []
+      row_prev = triangle[row_number - 1]
+      length = len(row_prev)+1
+      for i in range(length):
+        if i == 0:
+          row.append(1)
+        elif i > 0 and i < length-1:
+          row.append(triangle[row_number-1][i-1]+triangle[row_number-1][i])
+        else:
+          row.append(1)
+      triangle.append(row)
+      row_number += 1
+
+    for row in triangle:
+      print(row)
+
+pascal(1)
+pascal(2)
+pascal(3)
+pascal(4)
+pascal(5)
